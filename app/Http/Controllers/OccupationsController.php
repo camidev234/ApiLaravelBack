80c88ebadeaaab56b3054922913ea58ec9efc2ae
camidev234 <camilo222andresace@gmail.com>
2024-02-28 20:55:27 -0500

    Controller, models and migrations files created

---
+++ app/Http/Controllers/OccupationsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Occupations;
+use Illuminate\Http\Request;
+
+class OccupationsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Occupations $occupations)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Occupations $occupations)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Occupations $occupations)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Occupations $occupations)
+    {
+        //
+    }
+}
