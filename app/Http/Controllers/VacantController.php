a4220a4b2f8b34fa94a288225a52fe121ed66ec5
camidev234 <camilo222andresace@gmail.com>
2024-03-01 09:52:53 -0500

    Vacant controller created

--- app/Http/Controllers/VacantController.php
+++ app/Http/Controllers/VacantController.php
@@ -15,49 +15,23 @@ class VacantController extends Controller
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     */
+
     public function show(Vacant $vacant)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Vacant $vacant)
-    {
-        //
-    }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Vacant $vacant)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Vacant $vacant)
     {
         //
