1653f1a26adcb8f4f3799bff8627c887563f45be
camidev234 <camilo222andresace@gmail.com>
2024-03-01 09:51:21 -0500

    Other module deleted

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\CreateUserRequest;
 use App\Models\User;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
